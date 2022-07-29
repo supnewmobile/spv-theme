f933c5cc3c5e3976264691d643527fbda505b4d4
Francisco Beccuti <fbeccuti@simtlix.com>
2022-07-29 15:47:42 -0300

    virtual card.

--- lib/src/theme/supervielle_assets.dart
+++ lib/src/theme/supervielle_assets.dart
@@ -1,7 +1,7 @@
 class SupervielleAssets {
 
-  static const String visaLogo = 'lib/assets/vectors/logos/visa_logo.svg';
-  static const String visaDebitLogo = 'lib/assets/vectors/logos/visa_debit_logo.svg';
-  static const String mastercardLogo = 'lib/assets/vectors/logos/mastercard_logo.svg';
+  static const String visaLogo = 'packages/spv_theme/lib/assets/vectors/logos/visa_logo.svg';
+  static const String visaDebitLogo = 'packages/spv_theme/lib/assets/vectors/logos/visa_debit_logo.svg';
+  static const String mastercardLogo = 'packages/spv_theme/lib/assets/vectors/logos/mastercard_logo.svg';
 
 }
